6a3caba0758a86858bc9343d598fde39180e1696
lmorda <lou.morda@engilitycorp.com>
2017-12-20 10:04:45 -0800

    Added server sent event example

--- app/src/main/java/com/example/lmorda/websocketchat/WebSocketActivity.java
+++ app/src/main/java/com/example/lmorda/websocketchat/WebSocketActivity.java
@@ -1,6 +1,7 @@
 package com.example.lmorda.websocketchat;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
@@ -12,7 +13,7 @@ import okhttp3.WebSocket;
 import okhttp3.WebSocketListener;
 import okio.ByteString;
 
-public class MainActivity extends AppCompatActivity {
+public class WebSocketActivity extends AppCompatActivity {
 
     private TextView tvOutput;
 
@@ -23,13 +24,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tvOutput = findViewById(R.id.output);
+
+
+        // WebSocket
         Request request = new Request.Builder().url("ws://10.0.2.2:8080/websocket/chat").build();
         EchoWebSocketListener listener = new EchoWebSocketListener();
         OkHttpClient okHttpClient = new OkHttpClient();
-        WebSocket webSocket = okHttpClient.newWebSocket(request, listener);
+        final WebSocket webSocket = okHttpClient.newWebSocket(request, listener);
         okHttpClient.dispatcher().executorService().shutdown();
+
+        final Handler pingHandler = new Handler();
+        Runnable pingRunnable = new Runnable() {
+            @Override public void run() {
+                String ping = "{\"type\":\"ping\",\"message\":\"hello\"}";
+                output("Tx: " + ping);
+                webSocket.send(ping);
+                pingHandler.postDelayed(this, 10000);
+            }
+        };
+        pingHandler.postDelayed(pingRunnable, 10000);
     }
 
+    // WebSocket
     private final class EchoWebSocketListener extends WebSocketListener {
         @Override
         public void onOpen(WebSocket webSocket, Response response) {
@@ -45,7 +61,7 @@ public class MainActivity extends AppCompatActivity {
         public void onMessage(WebSocket webSocket, String text) {
             output("Rx: " + text);
             // Check the WebSocket message type
-            if(text.contains("chat")) {
+            if (text.contains("chat")) {
                 // do something chat related
             }
         }
